e3985779d950e4657ffaec3adb902544c498f434
Jakub Šturc <jakub.sturc@gmail.com>
2019-12-10 16:03:51 +0100

    Add Day10 part I

---
+++ Day10/Asteroid.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace JakubSturc.AdventOfCode2019.Day10
+{
+    [DebuggerDisplay("Asteroid = ({X},{Y})")]
+    public struct Asteroid
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Asteroid(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static double Angle(Asteroid from, Asteroid to)
+        {
+            var x = from.X - to.X;
+            var y = from.Y - to.Y;
+            return Math.Round(Math.Atan2(x, y), 5);
+        }
+
+        public static IEnumerable<Asteroid> ParseMap(string str)
+        {
+            int x = 1;
+            int y = 1;
+
+            foreach (char c in str)
+            {
+                switch (c)
+                {
+                    case '.': x++; continue;
+                    case '#': yield return new Asteroid(x, y); x++; continue;
+                    case '\n': y++; x = 1; continue;
+                    default: continue;
+                }
+            }
+
+        }
+
+        public static int CountVisible(Asteroid from, IEnumerable<Asteroid> map)
+        {
+            return map
+                .Where(a => !from.Equals(a))
+                .GroupBy(a => Angle(from, a))
+                .Count();
+        }
+
+        public static int IdealLocationIndex(IList<Asteroid> map)
+        {
+            return map.Select(a => CountVisible(a, map)).Max();
+        }
+    }
+}
---
+++ Day10/Tests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace JakubSturc.AdventOfCode2019.Day10
+{
+    using static Asteroid;
+
+    public class Tests
+    {
+        [Fact]
+        public void Parsing_Sample()
+        {
+            var asteroids = ParseMap(Sample1).ToList();
+
+            Assert.Equal(10, asteroids.Count);
+            Assert.Contains(new Asteroid(2, 1), asteroids);
+            Assert.Contains(new Asteroid(5, 5), asteroids);
+        }
+
+        [Fact]
+        public void Angle_Test()
+        {
+            var d = Angle(new Asteroid(1, 1), new Asteroid(1, 0));
+            Assert.Equal(0, d);
+        }
+
+        [Theory]
+        [InlineData(5,5, 7)]
+        [InlineData(4,5, 8)]
+        [InlineData(5,3, 5)]
+        public void Sample1_Visibility(int x, int y, int expected)
+        {
+            var map = ParseMap(Sample1).ToList();
+            var asteroid = new Asteroid(x, y);
+            var cnt = CountVisible(from: asteroid, map);
+            Assert.Equal(expected, cnt);
+        }
+
+        [Fact] 
+        public void Sample1_Result()
+        {
+            var map = ParseMap(Sample1).ToList();
+            Assert.Equal(8, IdealLocationIndex(map));
+        }
+
+        public const string Sample1 = @".#..#
+.....
+#####
+....#
+...##";
+
+    }
+}
