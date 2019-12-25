67be5be9f2da248f8cb5e1f851c32204af32895f
Jakub Šturc <jakub.sturc@gmail.com>
2019-12-25 23:08:12 +0100

    Part 1 - done

--- Day23/Program.cs
+++ Day23/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using JakubSturc.AdventOfCode2019.IntComp;
 
 namespace JakubSturc.AdventOfCode2019.Day23
@@ -12,86 +12,136 @@ namespace JakubSturc.AdventOfCode2019.Day23
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Starting ...");
+            Part1();
+        }
+        
+        static void Debug()
+        {
             var program = IntProgram.ParseFrom("input.txt");
+            var computer = new Computer(program, DummyInput(0));
 
-            var scheduler = new Scheduler(Size);
-            var network = new Network(Size);
-            var computers = new AsyncComputer[Size];
+            foreach ((var ticks, var output) in computer.Walk())
+            {
+                if (output.HasValue)
+                {
+                    Console.WriteLine($"{ticks};{output.Value}");
+                }
+            }
+
+            IEnumerable<long> DummyInput(int idx)
+            {
+                yield return idx;
+                while (true)
+                {
+                    yield return -1;
+                }
+            }
+        }
+        
+        static void Part1()
+        {
+            // Console.WriteLine("Ticks;From;To;X;Y");
+            var program = IntProgram.ParseFrom("input.txt");
+            var sw = Stopwatch.StartNew();
+            var computers = new Computer[Size];
+            var inputs = new Queue<long>[Size];
 
             for (int i = 0; i < Size; i++)
             {
-                computers[i] = new AsyncComputer(i, program, network, scheduler);
+                inputs[i] = new Queue<long>();
+                inputs[i].Enqueue(i);
+                computers[i] = new Computer(program, NetworkInput(i));
             }
 
-            scheduler.RunAll(computers);
+            var outputs = Interleave(computers.Select(c => c.Walk()));
 
-            Thread.Sleep(60_000);
+            var result = Route(outputs);
 
-            network.PrintStat();
+            Console.WriteLine($"Part I: {result} in {sw.ElapsedMilliseconds}ms");
 
-            //network.WaitForOutput();
-
-            //Console.WriteLine($"Part 1: {network.Result}");
-        }
-    }
-
-    public class Scheduler
-    {
-        private readonly int _size;
-        private readonly ManualResetEvent[] _resetEvents;
-        //private readonly AsyncComputer.PerfStat[] _stats;
-        private readonly Timer _timer;
-        private readonly int _processorCount;
-
-        private int _roundRobinCounter = 0;
-
-        public Scheduler(int size)
-        {
-            _size = size;
-            _processorCount = Environment.ProcessorCount;
-            _resetEvents = new ManualResetEvent[size];
-
-            for (int i = 0; i < _size; i++)
+            IEnumerable<long> NetworkInput(int idx)
             {
-                _resetEvents[i] = new ManualResetEvent(false);
+                var input = inputs[idx];
+
+                while (true)
+                {
+                    if (input.Count == 0)
+                    {
+                        yield return -1;
+                    }
+                    else
+                    {
+                        long val = input.Dequeue();
+                        yield return val;
+                    }
+                }
             }
 
-            _timer = new Timer(Schedule);
-        }
-
-        private void Schedule(object? _)
-        {
-            for (int i = 0; i < _processorCount; i++)
+            long Route(IEnumerable<(int comp, long output, long ticks)> outputs)
             {
-                var toStop = (_roundRobinCounter + i) % _size;
-                var resetEvent = _resetEvents[toStop];
-                resetEvent.Reset();
+                var packets = new Queue<long>[Size];
 
-                var toStart = (_roundRobinCounter + i + _processorCount) % _size;
-                resetEvent = _resetEvents[toStart];
-                resetEvent.Set();
+                for (int i = 0; i < Size; i++)
+                {
+                    packets[i] = new Queue<long>();
+                }
+
+                foreach ((int comp, long output, long ticks) in outputs)
+                {
+                    var packet = packets[comp];
+                    packet.Enqueue(output);
+
+                    if (packet.Count == 3)
+                    {
+                        int adr = (int)packet.Dequeue();
+                        long x = packet.Dequeue();
+                        long y = packet.Dequeue();
+
+                        //Console.WriteLine($"{ticks};{comp};{adr};{x};{y}");
+
+                        if (adr == 255) return y;
+
+                        var input = inputs[adr];
+                        input.Enqueue(x);
+                        input.Enqueue(y);
+                    }
+                }
+
+                return -1; // all computers ended without sending anything to 255
             }
         }
 
-        public void RunAll(AsyncComputer[] computers)
+        public static IEnumerable<(int comp, long output, long ticks)> Interleave(IEnumerable<IEnumerable<(long ticks, long? output)>> outputs)
         {
-            for (int i = 0; i < _size; i++)
+            var enumerators = outputs.Select(o => o.GetEnumerator()).ToArray();
+            var len = enumerators.Length;
+            var finished = new bool[len];
+            bool allFinished;
+
+            do
             {
-                var computer = computers[i];
-                //_stats[i] = computer.Stat;
-                var thread = new Thread(() => computer.Run());
-                thread.Start();
-            }
+                allFinished = true;
 
-            _timer.Change(0, 1000);
-        }
+                for (int i = 0; i < len; i++)
+                {
+                    if (finished[i]) continue;
 
-        public void WaitOne(AsyncComputer asyncComputer)
-        {   
-            var id = asyncComputer.Id;
-            var resetEvent = _resetEvents[id];
-            resetEvent.WaitOne();
+                    if (enumerators[i].MoveNext())
+                    {
+                        allFinished = false;
+                        (var ticks, var output) = enumerators[i].Current;
+                        if (output.HasValue)
+                        {
+                            yield return (comp: i, output: output.Value, ticks);
+                        }
+                    }
+                    else
+                    {
+                        finished[i] = true;
+                    }
+
+                }
+            } while (!allFinished);
         }
     }
 }
