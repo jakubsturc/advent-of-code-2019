d79a1f1869a70e929d0cba65cb2c611ae19af116
Jakub Šturc <jakub.sturc@gmail.com>
2019-12-26 00:10:40 +0100

    Day 23 - Part II

--- Day23/Program.cs
+++ Day23/Program.cs
@@ -27,9 +27,26 @@ namespace JakubSturc.AdventOfCode2019.Day23
 
             var outputs = Interleave(computers.Select(c => c.Walk()));
 
-            var result = Route(outputs);
+            var result = Route(outputs, part1: true);
 
             Console.WriteLine($"Part I: {result} in {sw.ElapsedMilliseconds}ms");
+
+            sw.Restart();
+
+            computers = new Computer[Size];
+            inputs = new Queue<long>[Size];
+
+            for (int i = 0; i < Size; i++)
+            {
+                inputs[i] = new Queue<long>();
+                inputs[i].Enqueue(i);
+                computers[i] = new Computer(program, NetworkInput(i));
+            }
+
+            outputs = Interleave(computers.Select(c => c.Walk()));
+            result = Route(outputs, part1: false);
+
+            Console.WriteLine($"Part II: {result} in {sw.ElapsedMilliseconds}ms");
 
             IEnumerable<long> NetworkInput(int idx)
             {
@@ -49,19 +66,41 @@ namespace JakubSturc.AdventOfCode2019.Day23
                 }
             }
 
-            long Route(IEnumerable<(int comp, long output, long ticks)> outputs)
+            long Route(IEnumerable<(int comp, long? output, long ticks)> outputs, bool part1)
             {
                 var packets = new Queue<long>[Size];
+                (long X, long Y) memory = (0L, 0L);
+                (long X, long Y) lastSend = (-1L, -1L);
+                const long idleTimeout = 1000;
+                long timestamp = 0; // ticks of the last routed pocket
 
                 for (int i = 0; i < Size; i++)
                 {
                     packets[i] = new Queue<long>();
                 }
 
-                foreach ((int comp, long output, long ticks) in outputs)
+                foreach ((int comp, long? output, long ticks) in outputs)
                 {
+                    if (part1 && !output.HasValue) continue;
+
+                    if (!part1 && ticks > timestamp + idleTimeout && packets.All(IsEmpty) && inputs.All(IsEmpty))
+                    {
+                        if (memory.Y == lastSend.Y)
+                        {
+                            return memory.Y;
+                        }
+
+                        var input = inputs[0];
+                        input.Enqueue(memory.X);
+                        input.Enqueue(memory.Y);
+                        lastSend = memory;
+                        timestamp = ticks;
+                    }
+
+                    if (!output.HasValue) continue;
+
                     var packet = packets[comp];
-                    packet.Enqueue(output);
+                    packet.Enqueue(output.Value);
 
                     if (packet.Count == 3)
                     {
@@ -71,19 +110,30 @@ namespace JakubSturc.AdventOfCode2019.Day23
 
                         //Console.WriteLine($"{ticks};{comp};{adr};{x};{y}");
 
-                        if (adr == 255) return y;
+                        if (adr == 255)
+                        {
+                            if (part1) return y;
+                            memory = (x, y);
+                        }
+                        else
+                        {
+                            var input = inputs[adr];
+                            input.Enqueue(x);
+                            input.Enqueue(y);
+                            
+                        }
 
-                        var input = inputs[adr];
-                        input.Enqueue(x);
-                        input.Enqueue(y);
+                        timestamp = ticks;
                     }
                 }
 
                 return -1; // all computers ended without sending anything to 255
             }
+
+            bool IsEmpty(Queue<long> q) => q.Count == 0;
         }
 
-        public static IEnumerable<(int comp, long output, long ticks)> Interleave(IEnumerable<IEnumerable<(long ticks, long? output)>> outputs)
+        public static IEnumerable<(int comp, long? output, long ticks)> Interleave(IEnumerable<IEnumerable<(long ticks, long? output)>> outputs)
         {
             var enumerators = outputs.Select(o => o.GetEnumerator()).ToArray();
             var len = enumerators.Length;
@@ -102,10 +152,7 @@ namespace JakubSturc.AdventOfCode2019.Day23
                     {
                         allFinished = false;
                         (var ticks, var output) = enumerators[i].Current;
-                        if (output.HasValue)
-                        {
-                            yield return (comp: i, output: output.Value, ticks);
-                        }
+                        yield return (comp: i, output, ticks);
                     }
                     else
                     {
